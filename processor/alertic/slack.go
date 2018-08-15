ccf972f3934b037207026d0bc1a0b541a7e6d545
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-08-15 16:15:58 +0700

    [andri/eko] add slack alertic file to handle message

--- processor/alertic/slack.go
+++ processor/alertic/slack.go
@@ -1,1 +1,32 @@
 package alertic
+
+import (
+	"strconv"
+	"bytes"
+	"net/http"
+	"github.com/prometheus/common/log"
+)
+
+func buildMessage(appName, logLevel string, currentCount int) (message string) {
+	return "{\"text\" :" + "\"" + appName + " had " + strconv.Itoa(currentCount) + " " + logLevel + "\"}"
+}
+
+func sendMessage(message, receiver string) {
+	_, err := http.Post(receiver, "application/json", bytes.NewBuffer([]byte(message)))
+
+	if err != nil {
+		log.Info(err.Error())
+	}
+
+	log.Info("Message sent!!")
+}
+
+func SendToSlack(appName, logLevel string, currentCount int, receivers []string) {
+	message := buildMessage(appName, logLevel, currentCount)
+
+	for idx, _ := range receivers {
+		receiver := receivers[idx]
+
+		sendMessage(message, receiver)
+	}
+}
